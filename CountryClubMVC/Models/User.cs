2d5ab488578ec73daf24073ccf56182d66acf772
Driaque <dan.onyeani@gmail.com>
2019-07-26 20:43:28 -0400

    -AdminLtecss Update PostList

--- CountryClubMVC/Models/User.cs
+++ CountryClubMVC/Models/User.cs
@@ -15,7 +15,6 @@ namespace CountryClubMVC.Models
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string Gender { get; set; }
-      
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Town { get; set; }
